d279f284677e0bdb33186cb5857f586b4387f646
jayasuryaC26 <jaysurya060604@gmail.com>
2024-07-25 16:36:34 +0530

    hiii

---
+++ Sura/src/com/tns/dayfour/Student.java
@@ -0,0 +1,11 @@
+package com.tns.dayfour;
+
+public class Student {
+	
+	public static void main(String[]args) {
+	 Student2 s=new Student2(2.3f);
+	 
+
+	}
+
+}
---
+++ Sura/src/com/tns/dayfour/Student2.java
@@ -0,0 +1,17 @@
+package com.tns.dayfour;
+
+public class Student2 {
+	Student2(){
+		System.out.println("Student");
+	}
+	Student2(int a){
+		System.out.println("the value of a is"+a);
+	}
+	Student2(float b){
+		System.out.println("the value of b is"+b);
+	}
+	Student2(int a,int b){
+		System.out.println("the value of a and b is"+a+b);
+	}
+
+}
